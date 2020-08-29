4b577448263feff5c7fdea5dbc22872c9821990e
talsadoum690 <yankhobadiop93@gmail.com>
2020-08-29 14:44:44 +0000

    Initial commit

---
+++ src/main/java/controller/LoginController.java
@@ -0,0 +1,24 @@
+package controller;
+
+
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+import java.awt.event.ActionEvent;
+
+public class LoginController {
+    @FXML
+    private TextField loginTfd;
+
+    @FXML
+    private PasswordField pwdPwf;
+
+    @FXML
+    void connexionHandle(ActionEvent event) {
+
+    }
+
+    public void connexionHandle(javafx.event.ActionEvent actionEvent) {
+    }
+}
